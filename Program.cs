d75a03c2ec4cdf77a114e5c5aa0f696de503514e
zemerovaanna <1zemerovaanna@gmail.com>
2023-02-04 10:46:36 +0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Вводим число ");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(n + new string('*', n));
+            Console.ReadKey();
+        }
+    }
+}
